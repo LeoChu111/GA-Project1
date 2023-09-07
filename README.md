17b975f16cbbdb1aeb6c9689c15e1bbfa07232d7
Wen-Hao Chu <leochu111@gmail.com>
2023-09-08 00:44:46 +1000

    Polish my code and add content to readme file

---
+++ README.md
@@ -0,0 +1,17 @@
+# Craps
+## Introduction
+Craps is a casino dice game that involves a great deal of action and strategy, both with money and the dice. The game of craps has many exciting aspects to it. The concept of playing craps is to roll a number (a point) with two dice and then roll that same number again before a seven is rolled. The person at the craps table that rolls the dice is called “the shooter.” When playing Craps, the most common craps bets are pass line, place bets, hard ways and craps & eleven.
+
+## ScreenShot
+
+
+## Technologies Used
+Javascript, html, CSS
+
+## Getting Started
+You can play by clicking [this](https://leochu111.github.io/GA-Project1/).
+
+## Next Steps
+In the future, new rules for craps might be add in, and better layout for 
+html page.
+
--- app.js
+++ app.js
@@ -2,12 +2,6 @@
 let dice1 
 let dice2 
 let rollCount = 0
-let userpoint = 0
-let placeToBet = [4,5,6,8,9,10]
-let playerBetNumber
-let playerBetAmount
-let depositAmount
-let exchangeAmount
 let winAmount
 const rollBtn = document.querySelector('.roll')
 const passlineBtn = document.querySelector('#passline')
@@ -16,21 +10,42 @@ const depositBtn = document.querySelector('.deposit')
 const exchangeBtn = document.querySelector('.exchange')
 const ruleBtn = document.querySelectorAll('.rule')
 const restartBtn = document.querySelector('.restart')
+const betBtn = document.querySelector('.betBtn')
+let userpoint = document.querySelector('.userpoint')
+let depositAmount = document.querySelector('.depositA')
+let exchangeAmount = document.querySelector('.exchangeA')
 let diceImage1 = document.getElementById("dice1")
 let diceImage2 = document.getElementById("dice2")
 let balance = document.querySelector('.balance')
 let cheque = document.querySelector('.cheque')
 let betAmount = document.querySelector('.betamount')
 let numberBet = document.querySelector('.betnumber')
+let playerBetNumber = document.querySelector('.betN')
+let playerBetAmount = document.querySelector('.betA')
+let result = document.querySelector('.result')
+let textPleaseBetNumber = document.querySelector('.text')
+let textPleaseBetAmount = document.querySelector('.text2')
+let chooseText = document.querySelector('.choose')
+let error = document.querySelector('#error')
+let error1 = document.querySelector('#error1')
+let error2 = document.querySelector('#error2')
+let error3 = document.querySelector('#error3')
+let error4 = document.querySelector('#error4')
 
 depositBtn.addEventListener('click', handleDeposit)
 exchangeBtn.addEventListener('click', handleExchange)
 rollBtn.addEventListener('click', handleRoll)
-passlineBtn.addEventListener('click', handlePassline)
+passlineBtn.addEventListener('click', handleDisplayPasslineUI)
+passlineBtn.addEventListener('click', function(){
+    betBtn.addEventListener('click', handlePassline)
+})
 passlineBtn.addEventListener('click', function(){
     rollBtn.addEventListener('click', handlePasslineResult)
 })
-placeBetBtn.addEventListener('click', handlePlaceBet)
+placeBetBtn.addEventListener('click', handleDisplayPlaceBetUI)
+placeBetBtn.addEventListener('click', function(){
+    betBtn.addEventListener('click',handlePlaceBet)
+})
 placeBetBtn.addEventListener('click', function(){
     rollBtn.addEventListener('click', handlePlaceBetResult)
 })
@@ -39,14 +54,22 @@ function disableButtons() {
     for(let button of ruleBtn) {
         button.style.display = 'none';
     }
+    betBtn.style.display = 'none'
 }
 function handleRoll() {
     for (let button of ruleBtn) {
         if(button.style.display !== 'none') {
-            alert('Please select rule first!')
+            error4.textContent = 'Please select rule first!'
+            error4.style.color = "red"
             return;
         }
     }
+    if (betBtn.style.display !== 'none') {
+        error4.textContent = 'Please select rule first!'
+        error4.style.color = "red"
+        return;
+    }
+    error4.textContent = ''
     rollCount += 1
     dice1 = Math.floor(Math.random() * 6) + 1
     dice2 = Math.floor(Math.random() * 6) + 1
@@ -79,151 +102,208 @@ function handleRoll() {
     }
 }
 function handleDeposit() {
-    depositAmount = prompt('Please enter amount you want to deposit')
-    if (isNaN(depositAmount) === true || depositAmount === '' || Number(depositAmount) < 0 || Number(depositAmount) % 1 !== 0){
-        alert('Please enter valid number')
+    if (depositAmount.value === '' || Number(depositAmount.value) < 0 || Number(depositAmount.value) % 1 !== 0){
+        error.textContent = 'Please enter valid number'
+        error.style.color = "red"
     } else {
-        balance.innerText = Number(balance.innerText) + Number(depositAmount)
+        balance.innerText = Number(balance.innerText) + Number(depositAmount.value)
+        error.textContent = ''
+        depositAmount.value = ''
     }
 }
 function handleExchange() {
-    exchangeAmount = prompt('Please enter amount you want to exchange')
-    if (isNaN(exchangeAmount) === true || exchangeAmount === '' || Number(exchangeAmount) < 0 || Number(exchangeAmount) > Number(balance.innerText) || Number(exchangeAmount) % 1 !== 0){
-        alert('Please enter valid number')
-    } else if (Number(balance.innerText) <  Number(exchangeAmount)) {
-        alert(`You don't have enough balance`)
-    } else if (Number(balance.innerText) >= Number(exchangeAmount)) {
-        cheque.innerText = Number(cheque.innerText) + Number(exchangeAmount)
-        balance.innerText = Number(balance.innerText) - Number(exchangeAmount)
+    if (exchangeAmount.value === '' || Number(exchangeAmount.value) < 0 || Number(exchangeAmount.value) > Number(balance.innerText) || Number(exchangeAmount.value) % 1 !== 0){
+        error1.textContent = 'Please enter valid number'
+        error1.style.color = "red"
+    } else if (Number(balance.innerText) <  Number(exchangeAmount.value)) {
+        error1.textContent = `You don't have enough balance`
+        error1.style.color = "red"
+    } else if (Number(balance.innerText) >= Number(exchangeAmount.value)) {
+        cheque.innerText = Number(cheque.innerText) + Number(exchangeAmount.value)
+        balance.innerText = Number(balance.innerText) - Number(exchangeAmount.value)
+        error1.textContent = ''
+        exchangeAmount.value = ''
     } 
 }
 function handlePlaceBet() {
     if(rollCount === 0) {
-        playerBetNumber = prompt('Please select number you want to bet')
-        if (isNaN(playerBetNumber) === true || playerBetNumber === '' || Number(playerBetNumber) < 0 || Number(playerBetNumber) % 1 !== 0) {
-            alert('Please enter valid number')
-        } else if (Number(playerBetNumber) === 4 || Number(playerBetNumber) === 5 || Number(playerBetNumber) === 9 || Number(playerBetNumber) === 10) {
-            playerBetAmount = prompt('Please enter how much you want to bet')
-            if(isNaN(playerBetAmount) === true || Number(playerBetAmount) % 1 !== 0) {
-                alert('Please enter valid number')
-            } else if (Number(playerBetAmount) < 5) {
-                alert('The minimum bet amount for 4,5,9,10 is 5')
-            } else if(playerBetAmount <= Number(cheque.innerText)) {
-                betAmount.innerText = Number(playerBetAmount)
-                cheque.innerText = Number(cheque.innerText) - Number(playerBetAmount)
-                numberBet.innerText = `You bet on number ${Number(playerBetNumber)}`
-                alert(`You have bet ${playerBetAmount} on Number ${playerBetNumber}`)
+        if (playerBetNumber.value === '' || Number(playerBetNumber.value) < 0 || Number(playerBetNumber.value) % 1 !== 0) {
+            error2.textContent = 'Please enter valid number'
+            error2.style.color = "red"
+        } else if (Number(playerBetNumber.value) === 4 || Number(playerBetNumber.value) === 5 || Number(playerBetNumber.value) === 9 || Number(playerBetNumber.value) === 10) {
+            error2.textContent = ''
+            if(Number(playerBetAmount.value) % 1 !== 0) {
+                error3.textContent = 'Please enter valid number'
+                error3.style.color = "red"
+            } else if (Number(playerBetAmount.value) < 5) {
+                error3.textContent ='The minimum bet amount for 4,5,9,10 is 5'
+                error3.style.color = "red"
+            } else if(Number(playerBetAmount.value) <= Number(cheque.innerText)) {
+                betAmount.innerText = Number(playerBetAmount.value)
+                cheque.innerText = Number(cheque.innerText) - Number(playerBetAmount.value)
+                numberBet.innerText = `You bet on number ${Number(playerBetNumber.value)}`
+                error3.textContent = ''
+                textPleaseBetAmount.style.display = 'none'
+                playerBetAmount.style.display = 'none'
+                textPleaseBetNumber.style.display = 'none'
+                playerBetNumber.style.display = 'none'
                 disableButtons()
             } else {
-                alert(`You don't have enough cheques to bet!`)
+                error3.textContent =`You don't have enough cheques to bet!`
+                error3.style.color = "red"
             }
-        } else if (Number(playerBetNumber) === 6 || Number(playerBetNumber) === 8) {
-            playerBetAmount = prompt('Please enter how much you want to bet')
-            if (Number(playerBetAmount) < 6) {
-                alert('The minimum bet amount for 6,8 is 6')
-            } else if(playerBetAmount <= Number(cheque.innerText)) {
-                betAmount.innerText = Number(playerBetAmount)
-                cheque.innerText = Number(cheque.innerText) - Number(playerBetAmount)
-                numberBet.innerText = `You bet on number ${Number(playerBetNumber)}`
-                alert(`You have bet ${playerBetAmount} on Number ${playerBetNumber}`)
+        } else if (Number(playerBetNumber.value) === 6 || Number(playerBetNumber.value) === 8) {
+            error2.textContent = ''
+            if (Number(playerBetAmount.value) < 6) {
+                error3.textContent = 'The minimum bet amount for 6,8 is 6'
+                error3.style.color = "red"
+            } else if(Number(playerBetAmount.value)<= Number(cheque.innerText)) {
+                betAmount.innerText = Number(playerBetAmount.value)
+                cheque.innerText = Number(cheque.innerText) - Number(playerBetAmount.value)
+                numberBet.innerText = `You bet on number ${Number(playerBetNumber.value)}`
+                error3.textContent = ''
+                textPleaseBetAmount.style.display = 'none'
+                playerBetAmount.style.display = 'none'
+                textPleaseBetNumber.style.display = 'none'
+                playerBetNumber.style.display = 'none'
                 disableButtons()
             } else {
-                alert(`You don't have enough cheques to bet!`)
+                error3.textContent =`You don't have enough cheques to bet!`
+                error3.style.color = "red"
             }
         } else {
-            alert('Please select one of the following number:4,5,6,8,9,10')
+            error2.textContent = 'Please select one of the following number:4,5,6,8,9,10'
         }
     } 
 }
 function handlePlaceBetResult() {
     if(rollCount > 1) {
-        if (Number(playerBetNumber) === 4 && (dice1 + dice2) === 4 || Number(playerBetNumber) === 10 && (dice1 + dice2) === 10) {
-            winAmount = Math.floor(Number(playerBetAmount) * 1.8)
+        if (Number(playerBetNumber.value) === 4 && (dice1 + dice2) === 4 || Number(playerBetNumber.value) === 10 && (dice1 + dice2) === 10) {
+            winAmount = Math.floor(Number(playerBetAmount.value) * 1.8)
             cheque.innerText = Number(cheque.innerText) + winAmount
             rollBtn.style.display = 'none'
             restartBtn.style.display ='inline'
-            alert(`You win $${winAmount}!`)
-        } else if (Number(playerBetNumber) === 5 && (dice1 + dice2) === 5 || Number(playerBetNumber) === 9 && (dice1 + dice2) === 9) {
-            winAmount = Math.floor(Number(playerBetAmount) * 1.4)
+            result.innerText = `$$$$ You win $${winAmount}! $$$$`
+        } else if (Number(playerBetNumber.value) === 5 && (dice1 + dice2) === 5 || Number(playerBetNumber.value) === 9 && (dice1 + dice2) === 9) {
+            winAmount = Math.floor(Number(playerBetAmount.value) * 1.4)
             cheque.innerText = Number(cheque.innerText) + winAmount
             rollBtn.style.display = 'none'
             restartBtn.style.display ='inline'
-            alert(`You win $${winAmount}!`)
-        } else if (Number(playerBetNumber) === 6 && (dice1 + dice2) === 6 || Number(playerBetNumber) === 8 && (dice1 + dice2) === 8) {
-            winAmount = Math.floor(Number(playerBetAmount) * 1.17)
+            result.innerText = `$$$$ You win $${winAmount}! $$$$`
+        } else if (Number(playerBetNumber.value) === 6 && (dice1 + dice2) === 6 || Number(playerBetNumber.value) === 8 && (dice1 + dice2) === 8) {
+            winAmount = Math.floor(Number(playerBetAmount.value) * 1.17)
             cheque.innerText = Number(cheque.innerText) + winAmount
             rollBtn.style.display = 'none'
             restartBtn.style.display ='inline'
-            alert(`You win $${winAmount}!`)
+            result.innerText = `$$$$ You win $${winAmount}! $$$$`
         } else if ((dice1 + dice2) === 7) {
             rollBtn.style.display = 'none'
             restartBtn.style.display ='inline'
-            alert(`You lose $${playerBetAmount}!`)
+            result.innerText = `$$$$ You lose $${playerBetAmount.value}! $$$$`
         }
     }
 }
+function handleDisplayPasslineUI() {
+    textPleaseBetAmount.style.display = 'block'
+    playerBetAmount.style.display = 'inline'
+    placeBetBtn.style.display = 'none'
+    passlineBtn.style.display = 'none'
+    betBtn.style.display = 'inline'
+    chooseText.style.display = 'none'
+}
+function handleDisplayPlaceBetUI() {
+    textPleaseBetAmount.style.display = 'block'
+    textPleaseBetNumber.style.display = 'block'
+    playerBetNumber.style.display = 'inline'
+    playerBetAmount.style.display = 'inline'
+    placeBetBtn.style.display = 'none'
+    passlineBtn.style.display = 'none'
+    betBtn.style.display = 'inline'
+    chooseText.style.display = 'none'
+}
 function handlePassline() {
     if(rollCount === 0) {
-        playerBetAmount = prompt('Please select amount you want to bet')
-        if (isNaN(playerBetAmount) === true || playerBetAmount === '' || Number(playerBetAmount) < 0 || Number(playerBetAmount) % 1 !== 0) {
-            alert('Please enter valid number')
-        } else if(Number(playerBetAmount) <= Number(cheque.innerText)) {
-            betAmount.innerText = Number(playerBetAmount)
-            cheque.innerText = Number(cheque.innerText) - Number(playerBetAmount)
-            alert(`You have bet ${playerBetAmount} on Passline`)
+        if (playerBetAmount.value === '' || Number(playerBetAmount.value) < 0 || Number(playerBetAmount.value) % 1 !== 0) {
+            error3.textContent = 'Please enter valid number'
+            error3.style.color = "red"
+        } else if(Number(playerBetAmount.value) <= Number(cheque.innerText)) {
+            betAmount.innerText = Number(playerBetAmount.value)
+            cheque.innerText = Number(cheque.innerText) - Number(playerBetAmount.value)
+            textPleaseBetAmount.style.display = 'none';
+            playerBetAmount.style.display = 'none';
+            numberBet.style.display = 'none';
+            error3.textContent = ''
             disableButtons()
         } else {
-            alert(`You don't have enough cheques to bet!`)
+            error3.textContent =`You don't have enough cheques to bet!`
+            error3.style.color = "red"
         }
     }
 }
 function handlePasslineResult() {
     if ((dice1 + dice2) === 7 && rollCount === 1|| (dice1 + dice2) === 11 && rollCount === 1) {
-        winAmount = Number(playerBetAmount) * 2
+        winAmount = Number(playerBetAmount.value) * 2
         cheque.innerText = Math.floor(Number(cheque.innerText) + winAmount)
         rollBtn.style.display = 'none'
         restartBtn.style.display ='inline'
-        alert(`You win $${winAmount}!`)
+        result.innerText = `$$$$ You win $${winAmount}! $$$$`
     } else if ((dice1 + dice2) === 2 && rollCount === 1|| (dice1 + dice2) === 3 && rollCount === 1|| (dice1 + dice2) === 12 && rollCount === 1)  {
         rollBtn.style.display = 'none'
         restartBtn.style.display ='inline'
-        alert(`You lose $${playerBetAmount}!`)
+        result.innerText = `$$$$ You lose $${playerBetAmount.value}! $$$$`
     } else if (rollCount === 1){
         //assign point to user's point
-        userpoint = dice1 + dice2
-        alert(`Now ${userpoint} is your bet`)
+        userpoint.innerText = `Now ${dice1 + dice2} is your bet`
     }
-    if (rollCount > 1 && (dice1 + dice2) === userpoint && (dice1 + dice2)!== 7) {
-        winAmount = Number(playerBetAmount) * 2
+    if (rollCount > 1 && (dice1 + dice2) === Number(userpoint.innerText) && (dice1 + dice2)!== 7) {
+        winAmount = Number(playerBetAmount.value) * 2
         cheque.innerText = Number(cheque.innerText) + winAmount
         rollBtn.style.display = 'none'
         restartBtn.style.display ='inline'
-        alert(`You win $${winAmount}!`)
+        result.innerText = `$$$$ You win $${winAmount}! $$$$`
     } else if (rollCount > 1 && (dice1 + dice2) === 7) {
         rollBtn.style.display = 'none'
         restartBtn.style.display ='inline'
-        alert(`You lose $${playerBetAmount}!`)
+        result.innerText = `$$$$ You lose $${playerBetAmount.value}! $$$$`
     }
 }
 function handleRestart() {
     numberBet.innerText = 'You bet on number: ?'
-    betAmount.innerText = 'Your bet amount: 0'
+    betAmount.innerText = '0'
     for(let button of ruleBtn) {
         button.style.display = 'inline';
     }
     rollBtn.style.display = 'inline'
-    playerBetAmount = ''
-    playerBetNumber = ''
+    playerBetAmount.value = ''
+    playerBetNumber.value = ''
+    result.innerText = ''
+    userpoint.innerText = ''
+    error.textContent = ''
+    error1.textContent = ''
+    error2.textContent = ''
+    error3.textContent = ''
     rollCount = 0
     rollBtn.removeEventListener('click', handlePlaceBetResult)
     rollBtn.removeEventListener('click', handlePasslineResult) 
-    placeBetBtn.addEventListener('click', handlePlaceBet)
+    betBtn.removeEventListener('click', handlePassline)
+    betBtn.removeEventListener('click', handlePlaceBet)
     placeBetBtn.addEventListener('click', function(){
         rollBtn.addEventListener('click', handlePlaceBetResult)
     })
     passlineBtn.addEventListener('click', function(){
         rollBtn.addEventListener('click', handlePasslineResult)
     })
+    diceImage1.src = ''
+    diceImage2.src = ''
+    chooseText.style.display = 'block'
+    textPleaseBetAmount.style.display = 'block';
+    playerBetAmount.style.display = 'inline';
+    numberBet.style.display = 'block';
+    textPleaseBetAmount.style.display = 'none'
+    playerBetAmount.style.display = 'none'
+    textPleaseBetNumber.style.display = 'none'
+    playerBetNumber.style.display = 'none'
     restartBtn.style.display = 'none';
+    
 }
 
